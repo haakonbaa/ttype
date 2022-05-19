2bcbae345f5507614ba4e99a780015ed9288256a
Håkon Bårsaune <haakonbaa@gmail.com>
2022-05-19 19:29:53 +0200

    add function for word generation

--- main.go
+++ main.go
@@ -3,11 +3,18 @@ package main
 import (
     "fmt"
     "os/exec"
+    "os"
+    "regexp"
+    "math/rand"
 )
 
 func main() {
     fmt.Println("Hello World")
-    highlightFile("test.go","cpp")
+    // highlightFile("test.go","cpp")
+    words := generateWords(5,"./words/english1000")
+    for i, v := range words {
+        fmt.Println(i,v)
+    }
 }
 
 func highlightFile(filename,language string) {
@@ -16,3 +23,23 @@ func highlightFile(filename,language string) {
     fmt.Println(string(out2))
     fmt.Println(out,err)
 }
+
+func random(min, max int) int {
+    return rand.Intn(max-min)+min
+}
+
+func generateWords(n int, filename string) []string {
+    reWord := regexp.MustCompile(`[a-zA-Z]+`)
+    strings := make([]string,n)
+    data, err := os.ReadFile(filename)
+    if err != nil {
+        panic(err)
+    }
+    allWordsInFile := reWord.FindAll(data,-1)
+    fmt.Println(len(allWordsInFile))
+    for i := 0; i < n; i++ {
+        strings[i] = string(allWordsInFile[rand.Intn(len(allWordsInFile)-1)])
+    }
+    return strings
+}
+
