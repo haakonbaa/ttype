5130b0baca9f0e16f7ec87b8e9cbd3c93192a033
Håkon Bårsaune <haakonbaa@gmail.com>
2022-05-25 20:50:44 +0200

    insert words and update screen

--- main.go
+++ main.go
@@ -8,20 +8,18 @@ import (
 func main() {
 	// disable input buffering
 	exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
-	// hide cursor
-	fmt.Printf("\033[?25l")
-	// hide input
-	//f mt.Printf("\033[8m")
+	fmt.Printf("\033[?25l") // hide cursor
+	//f mt.Printf("\033[8m") // hide input
 	// do not display entered characters on the screen
 	exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
 	// highlightFile("test.go","cpp")
 	words := generateWords(5, "./words/english1000")
-	for i, v := range words {
+	for i, v := range *words {
 		fmt.Println(i, v)
 	}
-	play()
-	for true {
-
-	}
-	readTest()
+	play(words)
+    fmt.Printf("\033[?25h")
+	fmt.Printf("\033[28m")
+    fmt.Printf("\033[0m") // reset
+    exec.Command("stty", "-F", "/dev/tty", "echo").Run()
 }
--- ttype.go
+++ ttype.go
@@ -23,7 +23,9 @@ type Word struct {
     split rune     // split between this and next word
 }
 
-func play() {
+const CLEARSCREEN = "\033[1;1H\033[2J"
+
+func play(words *[]Word) {
 	// buffer output
 	writer := bufio.NewWriter(os.Stdout)
 	writer.Flush()
@@ -40,6 +42,7 @@ func play() {
 			if sig == syscall.SIGINT {
 				fmt.Printf("\033[?25h")
 				fmt.Printf("\033[28m")
+                fmt.Printf("\033[0m")
 				exec.Command("stty", "-F", "/dev/tty", "echo").Run()
 				os.Exit(1)
 			}
@@ -50,18 +53,52 @@ func play() {
 			fmt.Println(sig)
 		}
 	}()
-	for true {
+    wordIndex := 0
+    insertedWords := make([]string,len(*words))
+    fmt.Println(insertedWords,wordIndex)
+	for wordIndex < len(*words) {
 		char, _, err := reader.ReadRune()
 		if err != nil {
 			panic(err)
 		}
+        if char == (*words)[wordIndex].split {
+            wordIndex += 1
+            if wordIndex >= len(*words) {
+                break
+            }
+        } else if char == rune('\x7f') {
+            // got backspace
+            if len(insertedWords[wordIndex]) > 0 {
+                insertedWords[wordIndex] = insertedWords[wordIndex][:len(insertedWords[wordIndex])-1]
+            }
+        } else {
+            insertedWords[wordIndex] = insertedWords[wordIndex] + string(char)
+        }
+        /*
 		if char == rune('\x7F') {
 			writer.WriteString(fmt.Sprintf("[<-]"))
 		} else {
 			writer.WriteString(fmt.Sprintf("[%c]", char))
 		}
+        */
+        // begin writing to screen
+        writer.WriteString(CLEARSCREEN)
+        for i := 0; i < len(*words); i++ {
+            if i < wordIndex {
+                formatWordErrors((*words)[i].wformat, insertedWords[i], writer)
+                writer.WriteString(string((*words)[i].split))
+            } else {
+                writer.WriteString((*words)[i].wformat)
+                writer.WriteString(string((*words)[i].split))
+            }
+        }
 		writer.Flush()
 	}
+}
+
+// Takes a word target and the user input and formats it writing to writer
+func formatWordErrors(target, input string, writer *bufio.Writer) {
+    writer.WriteString(input)
 }
 
 func highlightFile(filename, language string) {
@@ -75,7 +112,7 @@ func random(min, max int) int {
 	return rand.Intn(max-min) + min
 }
 
-func generateWords(n int, filename string) []Word {
+func generateWords(n int, filename string) *[]Word {
 	reWord := regexp.MustCompile(`[a-zA-Z]+`)
 	words := make([]Word, n)
 	data, err := os.ReadFile(filename)
@@ -91,5 +128,5 @@ func generateWords(n int, filename string) []Word {
             split: rune(' '),
         }
 	}
-	return words
+	return &words
 }
